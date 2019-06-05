df5b35efbf304e53b0662153cea63984af92025d
IceCreamYou <isaac@dfxmachina.com>
2019-06-05 09:25:19 -0700

    Updates dev dependencies

--- js/combined.js
+++ js/combined.js
@@ -1,5 +1,5 @@
 /**
- * HTML5 Canvas Game Boilerplate 2.2.0-08022019
+ * HTML5 Canvas Game Boilerplate 2.2.1-05062019
  * Certain components copyright their respective authors.
  *
  * @author Isaac Sukin (http://www.isaacsukin.com/)
